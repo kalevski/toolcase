613e13e39e83cde942af568bcef06ab65c736a92
kalevski <kalevski@outlook.com>
2022-03-14 20:09:34 +0100

    phaser-plus: exported Service class

--- phaser-plus/src/main.js
+++ phaser-plus/src/main.js
@@ -1,5 +1,7 @@
 import Scene from './Scene'
 import Module from './Module'
 import GameState from './GameState'
+import Service from './Service'
 
-export { Scene, Module, GameState }
+
+export { Scene, Module, GameState, Service }
