bef8e853027748c2b3189dd72b3699d82f6f2a83
kalevski <dakalevski@gmail.com>
2024-01-10 17:27:39 +0100

    state updated to support partial object for initial state

--- examples/src/StateExample.js
+++ examples/src/StateExample.js
@@ -2,15 +2,30 @@ import { State } from '@toolcase/base'
 
 class StateExample {
 
-    state = new State({
-        array1: [2, 3, 4],
-        array2: ['10', '11', '21']
-    })
+    state = new State({})
 
     run() {
         console.log(JSON.stringify(this.state.get()))
+        console.log('==== update 1 ====')
         this.state.set({
-            array1: [2]
+            array1: [2],
+            array2: { test: 1 }
+        })
+        console.log('==== update 2 ====')
+        this.state.set({
+            array2: undefined
+        })
+        console.log('==== update 3 ====')
+        this.state.set({
+            array2: [ 1, 2, 3 ]
+        })
+        console.log('==== update 4 ====')
+        this.state.set({
+            testObject: { testA: { childA: 1 }, testB: { childB: [ 1, 2 ] } }
+        })
+        console.log('==== update 5 ====')
+        this.state.set({
+            testObject: { testA: { childA: 2 } }
         })
         console.log(JSON.stringify(this.state.get()))
     }
